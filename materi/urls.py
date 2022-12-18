eb0131b8620465f10a536d1105f79a460dd37d01
firlindafadhilah <firlindafadhilahpriyantini@gmail.com>
2022-12-18 09:39:36 +0700

    keenam

--- materi/urls.py
+++ materi/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('about/', views.profile, name='about'),  
+    path('tentang/', views.tentang, name='tentang'),  
     path('pendahuluan/', views.pendahuluan, name='pendahuluan'),
     path('notasi/', views.notasi, name='notasi'),
     path('kosong/', views.kosong, name='kosong'),
@@ -12,7 +12,15 @@ urlpatterns = [
     path('operasi/', views.operasi, name='operasi'),
     path('diagram/', views.diagram, name='diagram'),
     path('kubus/', views.kubus, name='kubus'),
-    path('profile/', views.profile, name='profile'),
     path('mahasiswa/', views.mahasiswa, name='mahasiswa'),
+    path('kubus/', views.kubus, name='kubus'),
+    path('prisma/', views.prisma, name='prisma'),
+    path('limas/', views.limas, name='limas'),
+    path('balok/', views.balok, name='balok'),
+    path('substitusi/', views.substitusi, name='substitusi'),
+    path('eliminasi/', views.eliminasi, name='eliminasi'),
+    path('grafik/', views.grafik, name='grafik'),
+    path('perbedaan/', views.perbedaan, name='perbedaan'),
+    path('gabungan/', views.gabungan, name='gabungan'),
     
 ]
--- materi/views.py
+++ materi/views.py
@@ -7,8 +7,8 @@ from . models import Mahasiswa
 def index(request):
     template = loader.get_template('index.html')
     return HttpResponse(template.render())
-def about(request):
-    template = loader.get_template('about.html')
+def tentang(request):
+    template = loader.get_template('tentang.html')
     return HttpResponse(template.render())
 def pendahuluan(request):
     template = loader.get_template('pendahuluan.html')
@@ -34,9 +34,33 @@ def diagram(request):
 def kubus(request):
     template = loader.get_template('kubus.html')
     return HttpResponse(template.render())
-def profile(request):
-    template = loader.get_template('profile.html')
-    return HttpResponse(template.render())
 def mahasiswa(request):
     template = loader.get_template('mahasiswa.html')
+    return HttpResponse(template.render())
+def kubus(request):
+    template = loader.get_template('kubus.html')
+    return HttpResponse(template.render())
+def prisma(request):
+    template = loader.get_template('prisma.html')
+    return HttpResponse(template.render())
+def limas(request):
+    template = loader.get_template('limas.html')
+    return HttpResponse(template.render())
+def balok(request):
+    template = loader.get_template('balok.html')
+    return HttpResponse(template.render())
+def perbedaan(request):
+    template = loader.get_template('perbedaan.html')
+    return HttpResponse(template.render())
+def grafik(request):
+    template = loader.get_template('grafik.html')
+    return HttpResponse(template.render())
+def eliminasi(request):
+    template = loader.get_template('eliminasi.html')
+    return HttpResponse(template.render())
+def substitusi(request):
+    template = loader.get_template('substitusi.html')
+    return HttpResponse(template.render())
+def gabungan(request):
+    template = loader.get_template('gabungan.html')
     return HttpResponse(template.render())
